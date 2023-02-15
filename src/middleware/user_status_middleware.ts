ec61070bb6f4927453d48426d9e64144f2207d9b
danpinkB <belozarovichdanila08@gmail.com>
2023-02-15 17:09:01 +0100

    auth header

--- src/middleware/user_status_middleware.ts
+++ src/middleware/user_status_middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { verifyJwt } from "../utils/jwt";
 
 export default async function user_status_middleware(req:Request,res:Response,next:NextFunction){
-    let act:string = req.cookies.p_act;
+    let act:string = req.headers.authorization;
     console.log(act);
     
     if(!act){req['userAttr'] = {isAnonimus:true}; return next()}
