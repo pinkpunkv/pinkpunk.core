5f9f8065c2a25a6f744da2b709e6bf658ff13c8c
danpinkB <belozarovichdanila08@gmail.com>
2023-02-20 16:08:18 +0100

    f

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -7,10 +7,10 @@ enum RoleEnumType {
 
 export const createUserSchema = object({
   body: object({
-    username: string({
-      required_error: `{"code":"${CustomerErrorCode.Blank}","message":"username is required"}`,
+    // username: string({
+    //   required_error: `{"code":"${CustomerErrorCode.Blank}","message":"username is required"}`,
       
-    }),
+    // }),
     
     email: string({
       required_error: `{"code":"${CustomerErrorCode.Blank}","message":"email is required"}`,
