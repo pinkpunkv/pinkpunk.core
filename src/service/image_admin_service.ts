e60a9ce80d473edaee5aac716faa280687358eed
danpinkB <belozarovichdanila08@gmail.com>
2023-03-19 11:43:41 +0100

    images array

--- src/service/image_admin_service.ts
+++ src/service/image_admin_service.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client'
 import { HttpRequest } from "../common";
 import {StatusCodes} from 'http-status-codes'
 import { S3 } from '@aws-sdk/client-s3';
+import { BaseError } from '../exception';
 
 export default function make_image_admin_service(db_connection:PrismaClient,s3client:S3){
     return Object.freeze({
@@ -13,19 +14,21 @@ export default function make_image_admin_service(db_connection:PrismaClient,s3cl
     async function uploadImages(req:HttpRequest) {
         let images = [];
         return await db_connection.$transaction(async()=>{
+            if (req.files==null)
+                throw new BaseError(417,"files is null",[]);
             for (let i = 0; i < Object.entries(req.files).length; i++) {
                 let file:any = req.files[i];
                 let file_path = "/uploads/"+file.originalname
                 await s3client.putObject({ Bucket: process.env.S3_BUCKET_NAME, Key: file_path, Body: file.buffer, ACL: 'public-read', ContentType: file.mimetype })
                 images.push({url:file_path})
             }
-            await db_connection.image.createMany({
+            let images_ = await db_connection.image.createMany({
                 data:images
             })
             return {
                 status:StatusCodes.OK,
                 message:"success",
-                content: null
+                content: images_
             }
         })
     }
