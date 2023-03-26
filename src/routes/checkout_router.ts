641d38df69522f4453d8871e4697ec22adc4f3a4
danpinkB <belozarovichdanila08@gmail.com>
2023-03-26 14:12:25 +0200

    decrease

--- src/routes/checkout_router.ts
+++ src/routes/checkout_router.ts
@@ -8,7 +8,7 @@ checkout_router.get('/:checkoutId',m(checkout_service.getCheckout))
 checkout_router.post('/preprocess',m(checkout_service.preprocessCheckout))
 checkout_router.post('/:checkoutId',m(checkout_service.addToCheckout))
 checkout_router.put('/:checkoutId',m(checkout_service.updateCheckout))
-checkout_router.delete("/:checkoutId/decrease",m(checkout_service.decreaseCountFromCheckout))
+checkout_router.delete("/decrease/:checkoutId",m(checkout_service.decreaseCountFromCheckout))
 checkout_router.delete('/:checkoutId',m(checkout_service.removeVariantFromCheckout))
 
 export default checkout_router;
