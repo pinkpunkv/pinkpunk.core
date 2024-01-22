657988552ddc544a67067c5a3758f896a3e25e9f
belazp <belazproger@gmail.com>
2024-01-22 14:50:54 +0100

    filters

--- src/routes/product_router.ts
+++ src/routes/product_router.ts
@@ -7,9 +7,9 @@ product_router.get('/', cached(), product_service.get_products)
 product_router.get('/popular_new', cached(), product_service.get_popular_and_new_products)
 product_router.get('/path', cached(), product_service.get_products_pathes)
 product_router.get('/filters', cached(), product_service.get_filters)
+product_router.get('/search', product_service.search_products)
 product_router.get('/:slug', cached(), product_service.get_product_by_slug)
 
-product_router.get('/search', product_service.search_products)
 product_router.get('/:id/info', cached(), product_service.get_product)
 product_router.post('/:id/want', product_service.want_to)
 
