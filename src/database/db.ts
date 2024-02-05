8fc80eae7350a57bf3a64498bf343dc043d2d246
belazp <belazproger@gmail.com>
2024-02-05 14:31:24 +0100

    db importing fix

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, ProductPayload } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { createSoftDeleteMiddleware } from "prisma-soft-delete-middleware";
 
 let conection:PrismaClient|null;
