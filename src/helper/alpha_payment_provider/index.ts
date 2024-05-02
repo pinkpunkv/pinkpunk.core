c72300aa7927189d5f78b66f12856431c2d6fb3d
belazp <belazproger@gmail.com>
2024-05-02 13:40:45 +0200

    url fix

--- src/helper/alpha_payment_provider/index.ts
+++ src/helper/alpha_payment_provider/index.ts
@@ -6,24 +6,12 @@ import { PAYMENT_CALL_BACK_BASE_URL, PAYMENT_LOGIN, PAYMENT_PASSWORD, PAYMENT_UR
 const alpha_payment_service = {
     create_payment: async function create_payment(internal_order_id:number, payment_order_id: number, order_amount:Decimal,ct:string) {
         // let url = `${PAYMENT_URL}rest/register.do?userName=${PAYMENT_LOGIN}&password=${PAYMENT_PASSWORD}&orderNumber=${payment_order_id}&amount=${order_amount.toFixed(0)}&returnUrl=${PAYMENT_CALL_BACK_BASE_URL}/success?ct=${ct}&orderId=${internal_order_id}&failUrl=${PAYMENT_CALL_BACK_BASE_URL}/fail?ct=${ct}&orderId=${internal_order_id}`
-        return await axios.post(`${PAYMENT_URL}rest/register.do?
-            userName=${PAYMENT_LOGIN}&
-            password=${PAYMENT_PASSWORD}&
-            orderNumber=${payment_order_id}&
-            amount=${order_amount}&
-            returnUrl=${PAYMENT_CALL_BACK_BASE_URL}/success?ct=${ct}&orderId=${internal_order_id}&
-            failUrl=${PAYMENT_CALL_BACK_BASE_URL}/fail?ct=${ct}&orderId=${internal_order_id}`.trim()
-        )
+        return await axios.post(`${PAYMENT_URL}rest/register.do?userName=${PAYMENT_LOGIN}&password=${PAYMENT_PASSWORD}&orderNumber=${payment_order_id}&amount=${order_amount}&returnUrl=${PAYMENT_CALL_BACK_BASE_URL}/success?ct=${ct}&orderId=${internal_order_id}&failUrl=${PAYMENT_CALL_BACK_BASE_URL}/fail?ct=${ct}&orderId=${internal_order_id}`)
     },
 
     get_payment_status: async function get_payment_status(payment_order_id:number) {
         // let url = `${PAYMENT_URL}rest/getOrderStatusExtended.do?userName=${PAYMENT_LOGIN}&password=${PAYMENT_PASSWORD}&orderNumber=${payment_order_id}`;
-        return await axios.post(`
-            ${PAYMENT_URL}rest/getOrderStatusExtended.do?
-            userName=${PAYMENT_LOGIN}&
-            password=${PAYMENT_PASSWORD}&
-            orderNumber=${payment_order_id}
-        `.trim())
+        return await axios.post(`${PAYMENT_URL}rest/getOrderStatusExtended.do?userName=${PAYMENT_LOGIN}&password=${PAYMENT_PASSWORD}&orderNumber=${payment_order_id}`)
     },
 }
 export {alpha_payment_service}
